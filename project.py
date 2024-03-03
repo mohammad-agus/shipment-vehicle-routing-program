7f5cbe1ced842fd6afcc8ad5d44126f1b0904a7f
mohammad-agus <mohammadagus1st@gmail.com>
2024-03-03 22:16:33 +0800

    commit

--- project.py
+++ project.py
@@ -1,17 +1,74 @@
-import folium
+import folium, re, csv
 from openrouteservice.optimization import Job, Vehicle
 from openrouteservice import Client, convert
-import re
-import csv
 from typing import Union
 from datetime import datetime, date, time
-from Schedule import Schedule
 from tabulate import tabulate
 
 # Load API key
 from dotenv import dotenv_values
 
 api_key = dotenv_values(".env")["API_KEY"]
+
+
+class Schedule:
+
+    @classmethod
+    def to_unix_timestamp(cls, date_time: datetime) -> datetime.timestamp:
+        return date_time.timestamp()
+
+    @classmethod
+    def get_vehicle_time_window(
+        cls, vehicles: int, idx: int, datetime_to_epoch_function
+    ) -> list[datetime]:
+        p = r"^(2[0-9]{3})-0?([1-9]|1[0-2])-0?([1-9]|[1-3][0-9]) 0?([1-9]|[1-2][0-9]):0?([0-9]|[1-5][0-9])$"
+        while True:
+            try:
+                print('Date and time format: "YYYY-MM-DD HH:mm"')
+                if vehicles > 1:
+                    print(
+                        f"Input shipment schedule (date & time) of vehicle {idx+1} (in  format):"
+                    )
+                else:
+                    print(
+                        'Input shipment schedule (date & time) of the vehicle in "YYYY-MM-DD HH:mm" format:'
+                    )
+                dt1 = input(f"Begin: ")
+                dt1 = re.match(pattern=p, string=dt1)
+                if dt1 == None:
+                    print("Please input a valid datetime!")
+                    continue
+                ts1 = datetime(
+                    int(dt1.group(1)),
+                    int(dt1.group(2)),
+                    int(dt1.group(3)),
+                    int(dt1.group(4)),
+                    int(dt1.group(5)),
+                    0,
+                )
+                dt2 = input(f"End: ")
+                dt2 = re.match(pattern=p, string=dt2)
+                if dt2 == None:
+                    print("Please input a valid datetime!")
+                    continue
+                ts2 = datetime(
+                    int(dt2.group(1)),
+                    int(dt2.group(2)),
+                    int(dt2.group(3)),
+                    int(dt2.group(4)),
+                    int(dt2.group(5)),
+                    0,
+                )
+                if ts1 >= ts2:
+                    print("Invalid input: datetime beginning >= datetime ending!")
+                    continue
+                return [
+                    datetime_to_epoch_function(ts1),
+                    datetime_to_epoch_function(ts2),
+                ]
+            except ValueError:
+                print("Please input a valid datetime!")
+                pass
 
 
 def main():
@@ -157,8 +214,8 @@ def get_planned_amount(jobs: dict) -> int:
 
 def get_summary(result: dict, source: dict) -> None:
     # print("="*80)
-    print(" Routing Result ".center(50, "="))
-    headers = ["Summary", "Value"]
+    print("\n  Summary:")
+    headers = ["Result", "Value"]
     table = [
         ["Planned amount", get_planned_amount(get_job_list(source))],
         ["Delivery amount", result["summary"]["delivery"][0]],
@@ -179,9 +236,9 @@ def get_summary(result: dict, source: dict) -> None:
         )
     )
     for i, route in enumerate(result["routes"]):
-        print(f"Route {i+1}: Vehicle {route['vehicle']}".center(50, "-"))
+        print(f"\n>> Route {i+1}: Vehicle {route['vehicle']+1} ")
         for i, step in enumerate(route["steps"]):
-            type, location, load, arrival, duration, distance, job = (
+            type, location, load, arrival, duration, distance, job_id = (
                 step["type"],
                 step["location"],
                 step["load"],
@@ -192,16 +249,22 @@ def get_summary(result: dict, source: dict) -> None:
             )
             if step["type"] == "start":
                 print(
-                    f"{type.capitalize()}:\t {location}, load amount: {load[0]}, start time: {arrival}"
+                    f"{type.capitalize()}:\t{location}, load amount: {load[0]}, start time: {arrival}"
                 )
             elif step["type"] == "end":
                 print(
-                    f"{type.capitalize()}:\t {location}, load amount: {load[0]}, finish time: {arrival}"
+                    f"{type.capitalize()}:\t{location}, load amount: {load[0]}, finish time: {arrival}"
                 )
             else:
                 print(
-                    f"{type.capitalize()} {job+1}:\t {location}, load amount: {load[0]}, arrival time: {arrival}"
+                    f"{type.capitalize()} {job_id+1}:\t{location}, load amount: {load[0]}, arrival time: {arrival}"
                 )
+    print(f"\nUnassigned:")
+    for job in result["unassigned"]:
+        job_id = job["id"]
+        location = job["location"]
+        amount = get_job_list(source)[job_id].get("amount")[0]
+        print(f"Job {job_id+1}:\t{location}, amount: {amount}")
 
 
 if __name__ == "__main__":
